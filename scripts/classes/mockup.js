0a33b6a9478f4e80e19a19f54f3d65351fa09458
Matt Healy <matt@spoonandtrowel.com>
2013-05-26 12:12:41 -0400

    Add reset button

--- scripts/classes/mockup.js
+++ scripts/classes/mockup.js
@@ -1,20 +1,24 @@
 function Mockup() {
 
   this.init = function() {
+    this.$reset = $('#reset');
     this.render();
   },
 
   this.render = function() {
     this.disableOverscroll_();
+    this.decorate();
+  },
+
+  this.decorate = function() {
+    this.$reset.click(function() { location.reload(); });
   },
 
   this.disableOverscroll_ = function() {
-    // TODO(matt): find some other way to do this, eating touchmove events is shitty
+    // TODO(matt): find some other way to do this, eating touchmove events is not great
 
     document.body.addEventListener('touchmove', function(event) {
       event.preventDefault();
     }, false);
   }
-
-  this.init();
 };
